fca9cb1223cf9ab6bdd03b38ee52941c37357c25
alspezza <zspecian23@gmail.com>
2025-12-03 23:46:34 +0300

    Create __init__.py

---
+++ collision_package/collision/__init__.py
@@ -0,0 +1,13 @@
+from collision import (
+    isCollisionRect,
+    intersectionAreaRect,
+    intersectionAreaMultiRect,
+    RectCorrectError
+)
+
+__all__ = [
+    'isCollisionRect',
+    'intersectionAreaRect', 
+    'intersectionAreaMultiRect',
+    'RectCorrectError'
+]
